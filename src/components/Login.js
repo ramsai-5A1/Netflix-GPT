bdc268b0406f8bede238bdbe9dfc6f5c912b9a9e
Ramsai <ramsaicrony@gmail.com>
2024-01-15 17:55:07 +0530

    Tweaked sign-up and sign-in forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./Header";
 
+const buttonList = ["Sign In", "Sign Up"]
 const messagesList = ["New to Netflix? Sign up now", "Already had an account ? Login now"]
 
 const Login = () => {
@@ -18,10 +19,11 @@ const Login = () => {
             </div>
 
             <form className="rounded-sm w-3/12 absolute text-white bg-opacity-70 p-12 bg-black my-36 mx-auto left-0 right-0">
-                <h1 className=" font-bold text-3xl py-4">{isLoginForm ? "Sign in" : "Sign up"}</h1>
+                <h1 className=" font-bold text-3xl py-4">{isLoginForm ? buttonList[0] : buttonList[1]}</h1>
+                {!isLoginForm && <input className="rounded-sm p-4 my-4 w-full bg-slate-800" type="text" placeholder="Enter your full-name"/>}
                 <input className="rounded-sm p-4 my-4 w-full bg-slate-800" type="text" placeholder="Email or phone number"/>
                 <input className="rounded-sm p-4 my-4 w-full bg-slate-800" type="password" placeholder="Password"/>
-                <button className="rounded-lg p-5 my-6 bg-red-600 w-full">Sign In</button>
+                <button className="rounded-lg p-5 my-6 bg-red-600 w-full">{isLoginForm ? buttonList[0] : buttonList[1]}</button>
                 <div className="flex justify-between">
                     <span>Remember me</span>
                     <span>Need help?</span>
