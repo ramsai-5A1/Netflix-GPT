e36547147c713ee9b18d897a83529e699e3bd403
Ramsai <ramsaicrony@gmail.com>
2024-01-15 17:01:25 +0530

    Added background-image and logo on top-left corner

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,16 @@
+import Header from "./Header";
 
 const Login = () => {
     return (
         <div>
-            Login
+            <Header/>
+            <div className="">
+                <img
+                    alt="background"
+                    src="https://wallpapers.com/images/hd/netflix-background-gs7hjuwvv2g0e9fj.jpg"
+                />
+            </div>
+            
         </div>
     )
 };
