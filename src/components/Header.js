5fb05b5fba4269f4d0dc209a539f53e36ea9449e
Ramsai <ramsaicrony@gmail.com>
2024-01-15 17:46:32 +0530

    Added state variable to toggle between sign-in and login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 
 const Header = () => {
     return (
-        <div className="py-2 px-8 bg-gradient-to-b from-black absolute ">
+        <div className="z-10 py-2 px-8 bg-gradient-to-b from-black absolute ">
             <img
                 className="w-48"
                 src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import Header from "./Header";
 
+const messagesList = ["New to Netflix? Sign up now", "Already had an account ? Login now"]
+
 const Login = () => {
+
+    const [isLoginForm, setIsLoginForm] = useState(true);
+
     return (
         <div>
             <Header/>
-            <div className="">
+            <div className="absolute">
                 <img
                     alt="background"
                     src="https://wallpapers.com/images/hd/netflix-background-gs7hjuwvv2g0e9fj.jpg"
                 />
             </div>
+
+            <form className="rounded-sm w-3/12 absolute text-white bg-opacity-70 p-12 bg-black my-36 mx-auto left-0 right-0">
+                <h1 className=" font-bold text-3xl py-4">{isLoginForm ? "Sign in" : "Sign up"}</h1>
+                <input className="rounded-sm p-4 my-4 w-full bg-slate-800" type="text" placeholder="Email or phone number"/>
+                <input className="rounded-sm p-4 my-4 w-full bg-slate-800" type="password" placeholder="Password"/>
+                <button className="rounded-lg p-5 my-6 bg-red-600 w-full">Sign In</button>
+                <div className="flex justify-between">
+                    <span>Remember me</span>
+                    <span>Need help?</span>
+                </div>
+
+                <p className="py-5 hover:cursor-pointer" onClick={() => setIsLoginForm(!isLoginForm)}>{isLoginForm ? messagesList[0] : messagesList[1]}</p>
+            </form>
             
         </div>
     )
