59918031b56bcd5d90d7c84d2ac838cd01908204
Ramsai <ramsaicrony@gmail.com>
2024-01-16 18:41:31 +0530

    Added fix for browse page if user logged out

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import Header from "./Header";
+import {useSelector} from "react-redux"
+import { useEffect } from "react";
 
 const Browse = () => {
+
+    const navigate = useNavigate();
+    const user = useSelector((store) => store.user);
+
+    useEffect(() => {
+        if(!user) {
+            navigate("/");
+        }
+    }, []);
+    
+
     return (
         <div className="">
             <Header/> 
