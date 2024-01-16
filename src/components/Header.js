d21f99046377584d6d310400560f9586747d69d7
Ramsai <ramsaicrony@gmail.com>
2024-01-16 18:14:14 +0530

    Added log-out feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUserData } from "../utils/UserSlice";
 
 const Header = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const userData = useSelector((store) => store.user);
+
+    const handleSignOut = () => {
+        navigate("/");
+        dispatch(removeUserData());
+        console.log("Logged out successfully");
+    }
+
     return (
-        <div className="z-10 py-2 px-8 bg-gradient-to-b from-black absolute ">
+        <div className="z-10 w-screen py-2 px-8 bg-gradient-to-b from-black absolute flex justify-between">
             <img
                 className="w-48"
                 src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
                 alt="logo"
             />
+            {userData && <div className="flex p-2 justify-between">
+                <img
+                    className="w-12 h-12"
+                    alt="smilie-logo"
+                    src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-dyrp6bw6adbulg5b.jpg"
+                />
+                <button onClick={handleSignOut} className="bg-red-600 font-bold rounded-lg shadow-lg w-auto p-1 h-7">(Sign-out)</button>
+            </div>}
+            
         </div>
     )
 };
